837d121446599dc9d13a0d330357ecd7983064b0
SOHAIL AKHTAR <sohail-prodevans@users.noreply.github.com>
2018-10-24 15:57:34 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,10 +1,5 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-west-2b"
 }
