95f6cd1887560ae6aaaeae555d43da85b6c0128a
SOHAIL AKHTAR <sohail-prodevans@users.noreply.github.com>
2018-10-24 16:29:36 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = "${aws_access_key}"
+  secret_key = "${aws_secret_key}"
   region     = "us-west-2b"
 }
